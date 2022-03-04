7b55b7a0943dbb5f7f48e78dcdd185a9907e21ed
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-03-04 16:57:29 +0300

    first commit

---
+++ src/new.kt
@@ -0,0 +1,31 @@
+fun main() {
+    word("AkiraChix")
+    salutation("Charity", 2)
+   var word= count("Charity")
+    println(word)
+    recognize("Charity")
+
+}
+
+fun word(name:String){
+    var name="AkiraChix"
+    var newName=name[0].toString() + name[2] +name[3]
+    println(newName)
+}
+
+fun salutation(name:String, age:Int){
+    var statement= "Hi, my name is $name and I am $age years old."
+    println(statement)
+}
+
+fun count(name:String):Int {
+    var word = name.length
+    return word
+}
+fun recognize(name:String){
+    if(name=="Charity")
+        println("That's me")
+    else
+        println("I don't know who that is")
+}
+
