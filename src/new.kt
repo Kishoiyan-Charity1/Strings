cf638662c24d5bc1b5d27f04c5c16e41c1457381
Kishoiyan-Charity1 <kishoiyancharity@gmail.com>
2022-03-18 20:28:46 +0300

    first commit

--- src/new.kt
+++ src/new.kt
@@ -6,18 +6,18 @@ fun main() {
     recognize("Charity")
 
 }
-
+//function that print out the first, third and fourth characters
 fun word(name:String){
     var name="AkiraChix"
     var newName=name[0].toString() + name[2] +name[3]
     println(newName)
 }
-
+//function that takes in two parameter and return "hi, my name is x and i am y years old
 fun salutation(name:String, age:Int){
     var statement= "Hi, my name is $name and I am $age years old."
     println(statement)
 }
-
+//function that prints out the length of a string
 fun count(name:String):Int {
     var word = name.length
     return word
